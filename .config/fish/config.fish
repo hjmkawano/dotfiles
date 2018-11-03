018d846d07ae0c2ab92718622a6cd1b7ce458bd0
Hjm <hjmkawano@gmail.com>
2018-11-03 15:52:08 +0900

    fisherman v3 migration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,10 +3,25 @@ if test -n "$EMACS"
   set -x TERM eterm-color
 end
 
+
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+set fish_plugins theme git rbenv rails brew bundler gem osx pbcopy better-alias gi peco z tmux ghq spin fzf
+
 # this function may be required
 function fish_title
   true
 end
+
+alias git hub
+alias play-my-rock-list 'mpv --no-video --shuffle "https://www.youtube.com/playlist?list=PLvwAzdceyZtKzPv_EswzoRO0w1XFCW1m3"'
+alias play-my-band-maid-list 'mpv --no-video --shuffle "https://www.youtube.com/playlist?list=PLvwAzdceyZtJXQXePGfYchiEUDkZLLjhr"'
+
+set -U GHQ_SELECTOR fzf
 
 set -x GOPATH $HOME
 set -x PATH $HOME"/.local/bin" $PATH
@@ -27,12 +42,6 @@ status --is-interactive; and . (pyenv init -|psub)
 set -gx LDFLAGS " -L/usr/local/opt/zlib/lib"
 set -gx CPPFLAGS " -I/usr/local/opt/zlib/include"
 set -gx PKG_CONFIG_PATH "/usr/local/opt/zlib/lib/pkgconfig"
-
-alias git hub
-alias play-my-rock-list 'mpv --no-video --shuffle "https://www.youtube.com/playlist?list=PLvwAzdceyZtKzPv_EswzoRO0w1XFCW1m3"'
-alias play-my-band-maid-list 'mpv --no-video --shuffle "https://www.youtube.com/playlist?list=PLvwAzdceyZtJXQXePGfYchiEUDkZLLjhr"'
-
-set fish_plugins theme git rbenv rails brew bundler gem osx pbcopy better-alias gi peco z tmux ghq spin fzf
 
 set -g theme_powerline_fonts no
 set -g theme_nerd_fonts yes
