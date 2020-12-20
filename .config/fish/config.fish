1316cf15b7591f222bf517a2d4d86038af535412
Hjm Kwn <hjmkawano@gmail.com>
2020-12-20 15:25:29 +0900

    all reset

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,12 @@
 # emacs ansi-term support
-# if test -n "$EMACS"
-  # set -x TERM eterm-color
-# end
+if test -n "$EMACS"
+    set -x TERM eterm-color
+end
+
+# this function may be required
+function fish_title
+    true
+end
 
 set -x LANG en_US.UTF-8
 set -x LC_CTYPE $LANG
@@ -14,55 +19,66 @@ end
 
 set fish_plugins theme git rbenv rails brew bundler gem osx pbcopy better-alias z tmux ghq spin fzf
 
-# this function may be required
-function fish_title
-  true
-end
-
-# alias git hub
-# alias parrot 'docker run -ti --network bridge parrotsec/parrot'
 alias ps 'ps ax|fzf'
-alias mysql 'mysql --pager=less -S -n -i -F -X'
-
-
-set -x PATH $HOME"/.local/bin" $PATH
-set -x PATH $HOME"/bin" $PATH
-set -x PATH $HOME"/.yubikey/bin" $PATH
-
-set -x PATH "/usr/local/opt/coreutils/libexec/gnubin" $PATH
-set -x MANPATH "/usr/local/opt/coreutils/libexec/gnuman" $MANPATH
 
 set -x LESSCHARSET utf-8
-
 set -x GHQ_SELECTOR fzf
 
+# anyenv and XXenv
 status --is-interactive; and source (anyenv init -|psub)
 status --is-interactive; and source (pyenv init -|psub)
 status --is-interactive; and source (nodenv init -|psub)
 status --is-interactive; and source (goenv init -|psub)
+status --is-interactive; and source (rbenv init -|psub)
 set -x GOENV_DISABLE_GOPATH 1
 
 eval (direnv hook fish)
 
-set -gx LDFLAGS "-L/usr/local/opt/imagemagick@6/lib -L/usr/local/opt/nss/lib -L/usr/local/opt/qt/lib -L/usr/local/opt/zlib/lib"
-set -gx CPPFLAGS "-I/usr/local/opt/imagemagick@6/includ -I/usr/local/opt/nss/include -I/usr/local/opt/qt/include -I/usr/local/opt/zlib/include"
-set -x PKG_CONFIG_PATH "/usr/local/opt/imagemagick@6/lib/pkgconfig:/usr/local/opt/nss/lib/pkgconfig:/usr/local/Cellar/zlib/1.2.8/lib/pkgconfig:/usr/local/lib/pkgconfig:/opt/X11/lib/pkgconfig"
-
-set -g fish_user_paths "/usr/local/opt/nss/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/qt/bin" $fish_user_paths
+set -g fish_user_paths "~/.local/bin" $fish_user_paths
+set -g fish_user_paths "~/bin" $fish_user_paths
+set -g fish_user_paths "/~.yubikey/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/Cellar/git/2.29.2/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/coreutils/libexec/gnubin" $fish_user_paths
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/imagemagick@6/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/nss/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/qt/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/texlive/2018/bin/x86_64-darwin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/mysql-client/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/mysql@5.7/bin" $fish_user_paths
 
 set -g theme_powerline_fonts no
 set -g theme_nerd_fonts yes
 set -g theme_display_docker_machine yes
-set -g fish_prompt_pwd_dir_length 1
-set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+set -g fish_prompt_pwd_dir_length 2
 
-set -gx GNUPGHOME $HOME"/.gnupg"
+set -x VIRTUAL_ENV_DISABLE_PROMPT 1
+set -x GNUPGHOME $HOME"/.gnupg"
+set -x MANPATH "/usr/local/opt/coreutils/libexec/gnuman" $MANPATH
+
+function vterm_printf;
+    if [ -n "$TMUX" ]
+        # tell tmux to pass the escape sequences through
+        # (Source: http://permalink.gmane.org/gmane.comp.terminal-emulators.tmux.user/1324)
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
+
+if [ "$INSIDE_EMACS" = 'vterm' ]
+    function clear
+        vterm_printf "51;Evterm-clear-scrollback";
+        tput clear;
+    end
+end
+
+# startship (https://starship.rs/)
+starship init fish | source
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
