cbec85a94eb545dadf1a8e1d789aafc022186233
Hjm Kwn <hjmkawano@gmail.com>
2021-03-26 00:38:55 +0900

    [mod] Spacemacs dot config.
    - add packages and use-package
      - ssh-config-mode
      - counsel-web
    - Sync dot files template

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -243,6 +243,8 @@ This function should only modify configuration layer settings."
                      :fetcher github
                      :repo "canatella/xwwp"))
        itail
+       ssh-config-mode
+       counsel-web
        )
 
     ;; A list of packages that cannot be updated.
@@ -305,20 +307,29 @@ It should only modify the values of Spacemacs settings."
     ;; (default '(100000000 0.1))
     dotspacemacs-gc-cons '(200000000 0.1)
 
+    ;; Set `read-process-output-max' when startup finishes.
+    ;; This defines how much data is read from a foreign process.
+    ;; Setting this >= 1 MB should increase performance for lsp servers
+    ;; in emacs 27.
+    ;; (default (* 1024 1024))
+    dotspacemacs-read-process-output-max (* 1024 1024)
+
     ;; If non-nil then Spacelpa repository is the primary source to install
     ;; a locked version of packages. If nil then Spacemacs will install the
-    ;; latest version of packages from MELPA. (default nil)
-    dotspacemacs-use-spacelpa t
+    ;; latest version of packages from MELPA. Spacelpa is currently in
+    ;; experimental state please use only for testing purposes.
+    ;; (default nil)
+    dotspacemacs-use-spacelpa nil
 
     ;; If non-nil then verify the signature for downloaded Spacelpa archives.
-    ;; (default nil)
-    dotspacemacs-verify-spacelpa-archives nil
+    ;; (default t)
+    dotspacemacs-verify-spacelpa-archives t
 
     ;; If non-nil then spacemacs will check for updates at startup
     ;; when the current branch is not `develop'. Note that checking for
     ;; new versions works via git commands, thus it calls GitHub services
     ;; whenever you start Emacs. (default nil)
-    dotspacemacs-check-for-update t
+    dotspacemacs-check-for-update nil
 
     ;; If non-nil, a form that evaluates to a package directory. For example, to
     ;; use different package directories for different Emacs versions, set this
@@ -333,8 +344,10 @@ It should only modify the values of Spacemacs settings."
     ;; (emacslt 'vim)
     dotspacemacs-editing-style 'emacs
 
-    ;; If non-nil output loading progress in `*Messages*' buffer. (default nil)
-    dotspacemacs-verbose-loadinemacs nil
+    ;; If non-nil show the version string in the Spacemacs buffer. It will
+    ;; appear as (spacemacs version)@(emacs version)
+    ;; (default t)
+    dotspacemacs-startup-buffer-show-version t
 
     ;; Specify the startup banner. Default value is `official', it displays
     ;; the official spacemacs logo. An integer value is the index of text
@@ -1177,6 +1190,30 @@ before packages are loaded."
   (advice-add 'Info-find-node :around 'Info-find-node--info-ja)
 
 
+  (use-package counsel-web
+    :ensure t
+    :init
+    ;; Change the default browser
+    ;; To open the selected result in the operating system browser instead of Emacs:
+    (setq counsel-web-search-action #'eww)
+
+    ;; Change the alternate browser
+    (setq counsel-web-search-alternate-action #'browse-url)
+
+    ;; Change Search Engines
+    ;; DuckDuckGo and Google are built in. DuckDuckGo is the default. Change to Google like this:
+    ;; (setq counsel-web-engine 'google)
+
+    ;; Define "C-c w" as a prefix key.
+    (defvar counsel-web-map
+      (let ((map (make-sparse-keymap "counsel-web")))
+        (define-key map (kbd "w") #'counsel-web-suggest)
+        (define-key map (kbd "s") #'counsel-web-search)
+        (define-key map (kbd ".") #'counsel-web-thing-at-point)
+        map))
+    (global-set-key (kbd "C-c w") counsel-web-map)
+    )
+
   ;; (eval-after-load "eww"
   ;;   '(progn
   ;;      ;; 背景色の設定
@@ -1243,3 +1280,23 @@ before packages are loaded."
   ;;   ) ;; end of eww
 
   ) ;; end of user-config
+(defun dotspacemacs/emacs-custom-settings ()
+  "Emacs custom settings.
+This is an auto-generated function, do not modify its content directly, use
+Emacs customize menu instead.
+This function is called at the very end of Spacemacs initialization."
+  (custom-set-variables
+    ;; custom-set-variables was added by Custom.
+    ;; If you edit it by hand, you could mess it up, so be careful.
+    ;; Your init file should contain only one such instance.
+    ;; If there is more than one, they won't work right.
+    '(evil-want-Y-yank-to-eol nil)
+    '(package-selected-packages
+       '(counsel-web ssh-config-mode zenburn-theme zen-and-art-theme yasnippet-snippets yapfify yaml-mode xwwp-follow-link xwwp xterm-color ws-butler writeroom-mode wrap-region winum white-sand-theme which-key wgrep web-mode web-beautify vterm volatile-highlights vimrc-mode vi-tilde-fringe verb uuidgen use-package unfill undo-tree underwater-theme ujelly-theme twilight-theme twilight-bright-theme twilight-anti-bright-theme treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired toxi-theme toml-mode toc-org terminal-here tao-theme tangotango-theme tango-plus-theme tango-2-theme tagedit symon symbol-overlay sunny-day-theme sublime-themes subatomic256-theme subatomic-theme string-inflection string-edit sqlup-mode sql-indent sphinx-doc spaceline-all-the-icons spacegray-theme soothe-theme solarized-theme soft-stone-theme soft-morning-theme soft-charcoal-theme smyx-theme smex smeargle slim-mode slack shell-pop seti-theme seeing-is-believing scss-mode sass-mode rvm ruby-tools ruby-test-mode ruby-refactor ruby-hash-syntax rubocopfmt rubocop rspec-mode robe ripgrep reverse-theme reveal-in-osx-finder restart-emacs rebecca-theme realgud rbenv ranger rake rainbow-delimiters railscasts-theme pytest pyenv-mode py-isort purple-haze-theme pug-mode protobuf-mode professional-theme prettier-js popwin poetry plantuml-mode planet-theme pippel pipenv pip-requirements phoenix-dark-pink-theme phoenix-dark-mono-theme pdf-view-restore password-generator paradox pandoc-mode ox-rfc ox-qmd ox-pandoc ox-gfm ox-epub ox-asciidoc overseer osx-trash osx-dictionary osx-clipboard orgit-forge organic-green-theme org-superstar org-rich-yank org-recent-headings org-re-reveal org-ql org-projectile org-present org-pomodoro org-mime org-menu org-journal org-download org-cliplink org-brain org-books open-junk-file omtose-phellack-theme oldlace-theme occidental-theme obsidian-theme ob-translate ob-sql-mode ob-mongo ob-go ob-docker-build ob-async npm-mode nov nose nodejs-repl noctilux-theme naquadah-theme nameless mwim mustang-theme multi-term multi-line moom monokai-theme monochrome-theme molokai-theme moe-theme modus-vivendi-theme modus-operandi-theme mmm-mode minitest minimal-theme material-theme markdown-toc majapahit-theme magit-svn magit-section magit-gitflow madhat2r-theme macrostep lush-theme lsp-ui lsp-python-ms lsp-pyright lsp-origami lsp-ivy lorem-ipsum livid-mode live-py-mode link-hint light-soap-theme launchctl kaolin-themes json-navigator js2-refactor js-doc jq-mode jinja2-mode jenkinsfile-mode jenkins jbeans-theme jazz-theme japanese-holidays ivy-yasnippet ivy-xref ivy-purpose ivy-hydra ivy-avy itail ir-black-theme insert-shebang inkpot-theme indent-guide importmagic impatient-mode ietf-docs hybrid-mode hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation heroku-theme hemisu-theme helm-make hc-zenburn-theme gruvbox-theme gruber-darker-theme groovy-imports grip-mode grandshell-theme gotham-theme golden-ratio godoctor go-tag go-rename go-impl go-guru go-gen-test go-fill-struct go-eldoc go-autocomplete gnuplot gitignore-templates gitignore-mode github-search github-clone gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe+ gist gh-md gandalf-theme fuzzy font-lock+ flyspell-correct-ivy flycheck-pos-tip flycheck-package flycheck-elsa flycheck-bashate flx-ido flatui-theme flatland-theme fish-mode fish-completion farmhouse-theme fancy-battery eziam-theme eyebrowse expand-region exotica-theme exec-path-from-shell evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-textobj-line evil-surround evil-org evil-numbers evil-nerd-commenter evil-matchit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-goggles evil-exchange evil-escape evil-ediff evil-collection evil-cleverparens evil-args evil-anzu eval-sexp-fu espresso-theme eshell-z eshell-prompt-extras esh-help enh-ruby-mode engine-mode emr emoji-cheat-sheet-plus emmet-mode elisp-slime-nav ein editorconfig dumb-jump drag-stuff dracula-theme dotenv-mode doom-themes doom-modeline dockerfile-mode docker django-theme direnv dired-quick-sort diminish devdocs deft ddskk dash-at-point darktooth-theme darkokai-theme darkmine-theme darkburn-theme dap-mode dakrone-theme dactyl-mode cython-mode cyberpunk-theme csv-mode counsel-world-clock counsel-projectile counsel-osx-app counsel-dash counsel-css copy-as-format company-web company-statistics company-shell company-quickhelp company-lua company-go company-emoji company-box company-ansible company-anaconda column-enforce-mode color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized clues-theme clean-aindent-mode chruby chocolate-theme cherry-blossom-theme centered-cursor-mode busybee-theme bundler bubbleberry-theme browse-url-dwim browse-at-remote blacken birds-of-paradise-plus-theme badwolf-theme auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile apropospriate-theme anti-zenburn-theme ansible-doc ansible ample-zen-theme ample-theme all-the-icons-ivy-rich alect-themes aggressive-indent afternoon-theme ace-link ac-ispell)))
+  (custom-set-faces
+    ;; custom-set-faces was added by Custom.
+    ;; If you edit it by hand, you could mess it up, so be careful.
+    ;; Your init file should contain only one such instance.
+    ;; If there is more than one, they won't work right.
+    )
+  )
