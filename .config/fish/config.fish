5cbd99b20457e129fe9b1f28822f9e1dba51ada1
Hjm Kwn <hjmkawano@gmail.com>
2021-03-26 00:34:05 +0900

    [mod] Use Ruby macOS naively
    - modify $fish_user_paths
    - Alias ps to psf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,14 +14,15 @@ end
 
 set fish_plugins theme git rbenv rails brew bundler gem osx pbcopy better-alias z tmux ghq spin fzf
 
-alias ps 'ps ax|fzf'
+alias psf 'ps ax|fzf'
 
 set -x LESSCHARSET utf-8
 set -x GHQ_SELECTOR fzf
 
 set -g fish_user_paths "/usr/local/opt/python@3.8/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_path
+set -g fish_user_paths "/usr/local/lib/ruby/gems/3.0.0/bin" $fish_user_path
 set -g fish_user_paths "/usr/local/opt/mysql-client@5.7/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/texlive/2018/bin/x86_64-darwin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/qt/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/nss/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
@@ -32,18 +33,22 @@ set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/coreutils/libexec/gnubin" $fish_user_paths
 set -g fish_user_paths "/usr/local/Cellar/git/2.29.2/bin" $fish_user_paths
 set -g fish_user_paths "/~.yubikey/bin" $fish_user_paths
+set -g fish_user_paths "~/go/bin" $fish_user_paths
 set -g fish_user_paths "~/bin" $fish_user_paths
 set -g fish_user_paths "~/.local/bin" $fish_user_paths
 
-set -g theme_powerline_fonts no
+set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
 set -g theme_display_docker_machine yes
-set -g fish_prompt_pwd_dir_length 2
+set -g fish_prompt_pwd_dir_length 3
 
 set -x VIRTUAL_ENV_DISABLE_PROMPT 1
 set -x GNUPGHOME $HOME"/.gnupg"
 set -x MANPATH "/usr/local/opt/coreutils/libexec/gnuman" $MANPATH
 set -x RIPGREP_CONFIG_PATH $HOME"/.ripgreprc"
+
+# initialize direnv
+eval (direnv hook fish)
 
 # initialize anyenv and XXenv
 status --is-interactive; and source (anyenv init -|psub)
@@ -52,8 +57,6 @@ status --is-interactive; and source (nodenv init -|psub)
 status --is-interactive; and source (goenv init -|psub)
 status --is-interactive; and source (rbenv init -|psub)
 
-# initialize direnv
-eval (direnv hook fish)
 
 function vterm_printf;
     if [ -n "$TMUX" ]
