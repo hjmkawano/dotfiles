bca109c78a937e6c8b6011b02922f7a5161e231b
Hjm Kwn <hjmkawano@gmail.com>
2021-03-28 17:42:08 +0900

    [mod] Organized the order of the fish_user_path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,23 +19,25 @@ alias psf 'ps ax|fzf'
 set -x LESSCHARSET utf-8
 set -x GHQ_SELECTOR fzf
 
+set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/Cellar/git/2.29.2/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/python@3.8/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/ruby/bin" $fish_user_path
 set -g fish_user_paths "/usr/local/lib/ruby/gems/3.0.0/bin" $fish_user_path
-set -g fish_user_paths "/usr/local/opt/mysql-client@5.7/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/krb5/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/krb5/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/libpq/bin"  $fish_user_paths
+set -g fish_user_paths "/~.yubikey/bin" $fish_user_paths
+set -g fish_user_paths "~/.local/bin" $fish_user_paths
+set -g fish_user_paths "~/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/coreutils/libexec/gnubin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/imagemagick@6/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/mysql-client/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/qt/bin" $fish_user_paths
 set -g fish_user_paths "/usr/local/opt/nss/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/sqlite/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/imagemagick@6/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
-set -g fish_user_paths "/usr/local/opt/coreutils/libexec/gnubin" $fish_user_paths
-set -g fish_user_paths "/usr/local/Cellar/git/2.29.2/bin" $fish_user_paths
-set -g fish_user_paths "/~.yubikey/bin" $fish_user_paths
-set -g fish_user_paths "~/go/bin" $fish_user_paths
-set -g fish_user_paths "~/bin" $fish_user_paths
-set -g fish_user_paths "~/.local/bin" $fish_user_paths
 
 set -g theme_powerline_fonts yes
 set -g theme_nerd_fonts yes
