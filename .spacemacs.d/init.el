4c76a13f0c04cb72bdcf2dcc30f35c90c6400dce
Hjm Kwn <hjmkawano@gmail.com>
2021-04-08 16:50:07 +0900

    Use Nord theme

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -251,6 +251,7 @@ This function should only modify configuration layer settings."
        itail
        ssh-config-mode
        edit-indirect
+       nord-theme
        )
 
     ;; A list of packages that cannot be updated.
@@ -390,6 +391,7 @@ It should only modify the values of Spacemacs settings."
     ;; Press `SPC T n' to cycle to the next theme in the list (works great
     ;; with 2 themes variants, one dark and one light)
     dotspacemacs-themes '(
+                           nord
                            doom-nord
                            )
 
