6ea55544d6974067b8afcf7d280cbc328a5a1f0a
Hjm Kwn <hjmkawano@gmail.com>
2021-04-23 18:10:19 +0900

    update fish plugins

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,7 +1,5 @@
 set -q FZF_TMUX_HEIGHT; or set -U FZF_TMUX_HEIGHT "40%"
 set -q FZF_DEFAULT_OPTS; or set -U FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT"
-# set -q FZF_DEFAULT_COMMAND; or set -U FZF_DEFAULT_COMMAND "rg --hidden -l \"\""
-set -q FZF_DEFAULT_COMMAND; or set -U FZF_DEFAULT_COMMAND "rg --hidden -l ''"
 set -q FZF_LEGACY_KEYBINDINGS; or set -U FZF_LEGACY_KEYBINDINGS 1
 set -q FZF_DISABLE_KEYBINDINGS; or set -U FZF_DISABLE_KEYBINDINGS 0
 set -q FZF_PREVIEW_FILE_CMD; or set -U FZF_PREVIEW_FILE_CMD "head -n 10"
@@ -16,7 +14,7 @@ if test "$FZF_DISABLE_KEYBINDINGS" -ne 1
         bind \cg '__fzf_open'
         bind \co '__fzf_open --editor'
 
-        if bind -M insert >/dev/null 2>/dev/null
+        if ! test "$fish_key_bindings" = fish_default_key_bindings
             bind -M insert \ct '__fzf_find_file'
             bind -M insert \cr '__fzf_reverse_isearch'
             bind -M insert \ec '__fzf_cd'
@@ -32,7 +30,7 @@ if test "$FZF_DISABLE_KEYBINDINGS" -ne 1
         bind \eO '__fzf_open'
         bind \eo '__fzf_open --editor'
 
-        if bind -M insert >/dev/null 2>/dev/null
+        if ! test "$fish_key_bindings" = fish_default_key_bindings
             bind -M insert \co '__fzf_find_file'
             bind -M insert \cr '__fzf_reverse_isearch'
             bind -M insert \ec '__fzf_cd'
@@ -45,7 +43,7 @@ if test "$FZF_DISABLE_KEYBINDINGS" -ne 1
     if not bind --user \t >/dev/null 2>/dev/null
         if set -q FZF_COMPLETE
             bind \t '__fzf_complete'
-            if bind -M insert >/dev/null 2>/dev/null
+            if ! test "$fish_key_bindings" = fish_default_key_bindings
                 bind -M insert \t '__fzf_complete'
             end
         end
