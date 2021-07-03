949eca2f8fc3c31e4fb8ae51ad901875657c430f
Hjm Kwn <hjmkawano@gmail.com>
2021-07-03 17:34:23 +0900

    Add warning suppress for native-comp buffers.

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -1313,3 +1313,22 @@ before packages are loaded."
     )
 
   ) ;; end of user-config
+(defun dotspacemacs/emacs-custom-settings ()
+  "Emacs custom settings.
+This is an auto-generated function, do not modify its content directly, use
+Emacs customize menu instead.
+This function is called at the very end of Spacemacs initialization."
+  (custom-set-variables
+    ;; custom-set-variables was added by Custom.
+    ;; If you edit it by hand, you could mess it up, so be careful.
+    ;; Your init file should contain only one such instance.
+    ;; If there is more than one, they won't work right.
+    '(warning-suppress-log-types '((comp) (comp) (comp)))
+    '(warning-suppress-types '((comp) (comp))))
+  (custom-set-faces
+    ;; custom-set-faces was added by Custom.
+    ;; If you edit it by hand, you could mess it up, so be careful.
+    ;; Your init file should contain only one such instance.
+    ;; If there is more than one, they won't work right.
+    )
+  )
